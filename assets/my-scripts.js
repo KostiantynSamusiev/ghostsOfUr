6b5295308bd7ddf47fda3261715c48f2bbbfd945
KostiantynSamusiev <djfoozyhook@gmail.com>
2023-08-23 11:18:53 +0300

    update

--- assets/my-scripts.js
+++ assets/my-scripts.js
@@ -1,11 +1,31 @@
-
 $('.read-more').on("click", function () {
-    $('.about p').toggleClass("read-more-text", 1000, "easeOutSine");
+  $('.about p').toggleClass("read-more-text", 1000, "easeOutSine");
 });
 
 
-    $(document).ready(function(){
-      $('.gallery').slick({
-
-      });
-    });
+$(document).ready(function () {
+  $('.gallery').slick({
+    infinite: true,
+    slidesToShow: 2,
+    centerMode: true,
+    arrows: false,
+    responsive: [{
+        breakpoint: 990,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          infinite: true,
+          arrows: false,
+          centerMode: true,
+          slidesToShow: 2
+        }
+      }
+    ]
+  });
+});
